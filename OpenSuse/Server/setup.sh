27d6198e3673a0f3e5f32c4aef73f1160df70852
Woomymy <github@woomy.ovh>
2021-01-28 20:07:17 +0100

    🚧 Add Config Loader

--- OpenSuse/Server/setup.sh
+++ OpenSuse/Server/setup.sh
@@ -7,4 +7,9 @@ then
     echo -e "\e[91mSeems ${COMMON_LIB_PATH} doesn't exist... Have you \"Curl piped\" this script ? Git clone it or wget the archive..."
     exit 1
 fi
+# Load Opensuse scripts's shared library
 source "${COMMON_LIB_PATH}"
+# Load configurations
+source "${ROOT}/../../Config/Common.env" # Load default configs
+source "${ROOT}/../../Config/Opensuse.env"  # Load Opensuse's specific configs 
+# NOTE: Opensuse.env overwrites Common.env variables
