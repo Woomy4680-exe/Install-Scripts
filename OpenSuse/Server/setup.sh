1db4ba0241e73424e16bcee6e358d11e69b02de0
Woomymy <github@woomy.ovh>
2021-01-29 18:35:48 +0100

    🐛 Install $PACKAGES before installing cargo to fix "cc not found"

--- OpenSuse/Server/setup.sh
+++ OpenSuse/Server/setup.sh
@@ -14,7 +14,7 @@ source "${ROOT}/../../Config/Common.env" # Load default configs
 source "${ROOT}/../../Config/Opensuse.env"  # Load Opensuse's specific configs 
 # NOTE: Opensuse.env overwrites Common.env variables
 prepare_opensuse # Upgrade opensuse before
-install_cargo_packages # Install cargo, kelpdot and tab
 sudo zypper install -y "${PACKAGES}"
+install_cargo_packages # Install cargo, kelpdot and tab
 install_omz
 install_dots
